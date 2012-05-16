33d22db5bb8fa9499cb8a0ebb2452872d44b329c
Benjamin Eriksson <benjamin.eriksson@gmail.com>
2012-05-16 13:01:24 +0200

    adds dummy implementation for Image

---
+++ source/Renderer/Image.cpp
@@ -0,0 +1,7 @@
+#include <Renderer/Image.h>
+
+using namespace stingray_renderer;
+
+void Image::store(const std::string& path)
+{
+}
---
+++ source/Renderer/Image.h
@@ -0,0 +1,15 @@
+#ifndef STINGRAY_IMAGE_H
+#define STINGRAY_IMAGE_H
+
+#include <string>
+
+namespace stingray_renderer
+{
+	class Image
+	{
+	public:
+		void store(const std::string& path);
+	};
+}
+
+#endif
