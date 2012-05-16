c53d448af841cee2458d31b63d32ec5f56927356
Benjamin Eriksson <benjamin.eriksson@gmail.com>
2012-05-16 07:29:57 +0200

    cleaning up namespace uses

--- source/Scene/Scene.cpp
+++ source/Scene/Scene.cpp
@@ -4,40 +4,39 @@
 #include <Renderer/Material.h>
 
 using namespace stingray_math;
+using namespace stingray_scene;
 
-namespace stingray_scene
+Scene::Scene()
 {
-	Scene::Scene()
-	{
 
-	}
-
-	Scene::~Scene()
-	{
-
-	}
-
-	void Scene::loadSceneFromFile(std::string path)
-	{
-		// Load default scene
-		if (path == "")
-		{
-			AddShape(new Sphere(new Vec3(0.0, 0.0, 0.0), 
-			                    1.0, 
-			                    new stingray_renderer::Material(
-								   new Vec4(1.0, 0.0, 0.0, 0.0), 0.0, 0.0)));
-			AddShape(new Sphere(new Vec3(0.0, 1.5, 1.5), 
-			                    0.5,
-								new stingray_renderer::Material(
-								   new Vec4(0.0, 1.0, 0.0, 0.0), 0.0, 0.0)));
-			SetCamera(new Camera(new Vec3(-1.5, -1.5, -1.5), 
-			                     new Vec3(1.0, 1.0, 1.0), 
-								 1.5 * MATH_PI));
-		}
-		// Non-default scene is not supported
-		else
-		{
-			std::cerr << "DON'T TRY TO LOAD NON-DEFAULT SCENES, JACKASS"  << std::endln;
-		}
-	}	
 }
+
+Scene::~Scene()
+{
+
+}
+
+void Scene::loadSceneFromFile(std::string path)
+{
+	// Load default scene
+	if (path == "")
+	{
+		AddShape(new Sphere(new Vec3(0.0, 0.0, 0.0), 
+		                    1.0, 
+		                    new stingray_renderer::Material(
+							   new Vec4(1.0, 0.0, 0.0, 0.0), 0.0, 0.0)));
+		AddShape(new Sphere(new Vec3(0.0, 1.5, 1.5), 
+		                    0.5,
+							new stingray_renderer::Material(
+							   new Vec4(0.0, 1.0, 0.0, 0.0), 0.0, 0.0)));
+		SetCamera(new Camera(new Vec3(-1.5, -1.5, -1.5), 
+		                     new Vec3(1.0, 1.0, 1.0), 
+							 1.5 * MATH_PI));
+	}
+	// Non-default scene is not supported
+	else
+	{
+		std::cerr << "DON'T TRY TO LOAD NON-DEFAULT SCENES, JACKASS"  << std::endln;
+	}
+}
+
